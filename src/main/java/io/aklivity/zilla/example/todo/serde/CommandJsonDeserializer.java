7b9ba9ce635b78307511479410a8b627b000467c
Akram  Yakubov <akram.yakubov@gmail.com>
2022-05-22 21:14:45 -0700

    Log missing idempotency key header in a case user misses

--- src/main/java/io/aklivity/zilla/example/todo/serde/CommandJsonDeserializer.java
+++ src/main/java/io/aklivity/zilla/example/todo/serde/CommandJsonDeserializer.java
@@ -30,9 +30,10 @@ public class CommandJsonDeserializer implements Deserializer<Command>
     {
         final Header domainModelHeader = headers.lastHeader("zilla:domain-model");
         final Header correlationId = headers.lastHeader("zilla:correlation-id");
+        final Header idempotencyKey = headers.lastHeader("idempotency-key");
         final String domainModel = new String(domainModelHeader.value());
 
-        if (correlationId != null)
+        if (correlationId != null && idempotencyKey != null)
         {
             JavaType type = null;
             switch (domainModel)
